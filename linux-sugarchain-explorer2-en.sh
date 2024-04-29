b672e2c3ae2167f70276c6c895686e6542878e04
bailaoshi <449708913@qq.com>
2024-04-29 22:21:42 +0800

    Create linux-sugarchain-explorer2-en.sh

---
+++ linux-sugarchain-explorer2-en.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+#disable bash history
+set +o history
+
+#color codes
+RED='\033[1;31m'
+YELLOW='\033[1;33m'
+BLUE="\\033[38;5;27m"
+SEA="\\033[38;5;49m"
+GREEN='\033[1;32m'
+CYAN='\033[1;36m'
+NC='\033[0m'
+dversion="v1.0"
+CRTDIR=$(pwd)
+
+
+function string_limit_check_mark(){
+	if [[ -z "$2" ]]; then
+		string="$1"
+		string=${string::100}
+	else
+		string=$1
+		string_color=$2
+		string_leght=${#string}
+		string_leght_color=${#string_color}
+		string_diff=$((string_leght_color-string_leght))
+		string=${string_color::100+string_diff}
+	fi
+	echo -e "${ARROW}${CYAN}$string[${CHECK_MARK}√${CYAN}]${NC}"
+}
+
+function string_limit_x_mark(){
+	if [[ -z "$2" ]]; then
+		string="$1"
+		string=${string::50}
+	else
+		string=$1
+		string_color=$2
+		string_leght=${#string}
+		string_leght_color=${#string_color}
+		string_diff=$((string_leght_color-string_leght))
+		string=${string_color::50+string_diff}
+	fi
+	echo -e "${ARROW}${CYAN}$string[${CHECK_MARK}${RED}×${CYAN}]${NC}"
+}
+
+function Start_sugar_explorer(){
+	#install nvm
+	if ! nvm --version > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing nvm ....${NC}"
+		unset NVM_DIR
+		cd
+		curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+		source /etc/profile
+		profile_LINE="NVM_DIR/nvm.sh"
+		profile_EXISTS=$(cat /etc/profile | grep "$profile_LINE" | wc -l)
+		if [ $profile_EXISTS -eq 0 ]
+		then
+			echo 'export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"  # This loads nvm bash_completion' >> /etc/profile
+			source /etc/profile
+			string_limit_check_mark "Write successful........." "Write successful${GREEN}${CYAN} ........."
+		fi
+		source /etc/profile
+	fi
+
+	#install node
+	if ! node -v > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing node ....${NC}"
+		nvm install v12.14.0
+	fi
+	
+	if [[ ! -d ${CRTDIR}/sugarchain-blockchain-explorer2 ]]; then
+		git clone https://github.com/bailaoshijiadao/sugarchain-blockchain-explorer2
+	fi
+	
+	pm2_flag=`pm2 list`
+	if [[ $pm2_flag =~ "sugarchain-blockchain-explorer2" ]]; then
+		echo -e "${ARROW} ${YELLOW}sugarchain-blockchain-explorer2 already exists....${NC}"
+	else
+		cd sugarchain-blockchain-explorer2
+		npm install
+		npm install pm2 -g
+		pm2 start ./bin/www --name sugarchain-blockchain-explorer2
+	fi
+}
+
+function install_step(){
+
+	if ! figlet -v > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}安装 figlet 中 ....${NC}"
+		sudo $* install -y figlet > /dev/null 2>&1
+	fi
+	
+	#install JQ
+	if ! jq --version > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing JQ ....${NC}"
+		sudo $* install -y jq > /dev/null 2>&1
+	fi
+
+	if ! screen -v > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing screen ....${NC}"
+		sudo $* install -y screen > /dev/null 2>&1
+	fi
+	
+	if ! wget --version > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing wget ....${NC}"
+		sudo $* install -y wget > /dev/null 2>&1
+	fi
+	
+	if ! git --version > /dev/null 2>&1; then
+		echo -e "${ARROW} ${YELLOW}Installing git ....${NC}"
+		sudo $* install -y git > /dev/null 2>&1
+	fi
+}
+
+if ! node -version > /dev/null 2>&1 ; then
+	#Ubuntu
+	if [[ -f /etc/issue ]]; then
+		system_v=$(cat /etc/issue)
+		if [[ $system_v =~ "Ubuntu" ]]; then
+			echo -e "${YELLOW}apt${NC}"
+			sudo apt-get update
+			install_step apt
+		fi
+	fi
+
+	#Centos
+	if [[ -f /etc/centos-release ]]; then
+		echo -e "${YELLOW}yum${NC}"
+		install_step yum
+	else 
+		if [[ -f /etc/redhat-release ]]; then
+			echo -e "${YELLOW}yum${NC}"
+			install_step yum
+		fi
+	fi
+
+	#Centos换源
+	if [[ -f /etc/centos-release ]]; then
+		if ! jq --version > /dev/null 2>&1; then
+			mv /etc/yum.repos.d /etc/yum.repos.d.bak
+			mkdir -p /etc/yum.repos.d
+			curl https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo > /etc/yum.repos.d/Centos-vault-8.5.2111.repo
+			curl https://mirrors.aliyun.com/repo/epel-archive-8.repo > /etc/yum.repos.d/epel-archive-8.repo
+			yum clean all && yum makecache
+		fi
+		install_step yum
+	else 
+		if [[ -f /etc/redhat-release ]]; then
+			if ! jq --version > /dev/null 2>&1; then
+				mv /etc/yum.repos.d /etc/yum.repos.d.bak
+				mkdir -p /etc/yum.repos.d
+				curl https://mirrors.aliyun.com/repo/Centos-vault-8.5.2111.repo > /etc/yum.repos.d/Centos-vault-8.5.2111.repo
+				curl https://mirrors.aliyun.com/repo/epel-archive-8.repo > /etc/yum.repos.d/epel-archive-8.repo
+				yum clean all && yum makecache
+			fi
+			install_step yum
+		fi
+	fi
+fi
+
+while :
+do
+	sleep 1
+	echo -e "${BLUE}"
+	figlet -f big "SugarChain"
+	echo -e "${YELLOW}===========================================================${NC}"
+	echo -e "${GREEN}Version: $dversion${NC}"
+	echo -e "${GREEN}System: Linux Ubuntu Centos${NC}"
+	echo -e "${GREEN}Author: bailaoshi${NC}"
+	echo -e "${YELLOW}===========================================================${NC}"
+	echo -e "${CYAN}1  - Start SugarChain Blockchain Explorer2[Including installation process]${NC}"
+	echo -e "${CYAN}2  - View the usage of blockchain explorer2 resources${NC}"
+	echo -e "${CYAN}3  - Delete blockchain explorer2 PM2 application${NC}"
+	echo -e "${CYAN}4  - Blockchain explorer2 startOrReload${NC}"
+	echo -e "${CYAN}5  - View Blockchain explorer2 logs${NC}"
+	echo -e "${CYAN}6  - View Blockchain explorer2 details${NC}"
+	echo -e "${CYAN}7  - Stop SugarChain Blockchain explorer2${NC}"
+	echo -e "${YELLOW}===========================================================${NC}"
+	echo -e "${YELLOW}******SugarChain Blockchain explorer2 script******${NC}"
+	echo -e "${YELLOW}Sponsorship address: sugar1q8d79pk0jyhl92jrmfsrpvuwwg9ycsr20xz93rz(bailaoshi)${NC}"
+
+	read -rp "Please enter a numerical option and press Enter: "
+
+	  case "$REPLY" in
+
+	 1)
+		sleep 2
+		Start_sugar_explorer
+	 ;;
+	 2)
+
+		sleep 2
+		pm2 monit
+	 ;;
+	 3) 
+
+		sleep 2
+		pm2 delete sugarchain-blockchain-explorer2
+	 ;;
+	 4) 
+
+		sleep 2
+		pm2 startOrReload sugarchain-blockchain-explorer2
+	 ;;
+	 5) 
+
+		sleep 2
+		pm2 logs sugarchain-blockchain-explorer2
+	 ;;
+	 6) 
+
+		sleep 2
+		pm2 show sugarchain-blockchain-explorer2
+	 ;;
+	 7)
+		sleep 2
+		pm2 stop sugarchain-blockchain-explorer2
+	 ;;
+
+		esac
+
+done
+
